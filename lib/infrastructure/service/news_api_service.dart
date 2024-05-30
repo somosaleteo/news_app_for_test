b83178c4e226c3d96911b3a25420376ff48ce236
Yeimy Alejandra Rodriguez Ruiz <alejandra.rodriguez@pragma.com.co>
2024-05-30 15:32:00 -0500

    se agrega extension de HttpOverrides

--- lib/infrastructure/service/news_api_service.dart
+++ lib/infrastructure/service/news_api_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-
+import 'package:news_app_for_test/infrastructure/extensions/http_overrides_extension.dart';
+import 'dart:io';
 import '../models/news_model.dart';
 
 class NewsApiService {
@@ -8,12 +9,12 @@ class NewsApiService {
   static const String _baseUrl = 'https://newsapi.org/v2';
 
   Future<List<NewsModel>> fetchTopHeadlines() async {
+    HttpOverrides.global = HttpOverridesExtension();
     final response = await http
         .get(Uri.parse('$_baseUrl/top-headlines?country=us&apiKey=$_apiKey'));
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
-      print(data);
       final List articles = data['articles'];
       final news = articles.map((json) => NewsModel.fromJson(json)).toList();
       return news;
